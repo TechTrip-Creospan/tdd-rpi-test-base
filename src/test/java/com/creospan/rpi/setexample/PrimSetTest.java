03048034aecde0dfe3d4ead67e808cf93c3246cb
Terry Trippany <terry.trippany@gmail.com>
2021-10-30 16:06:03 -0500

    Adding a class to demonstrate converting the set to use generic types

---
+++ src/test/java/com/creospan/rpi/setexample/PrimSetTest.java
@@ -0,0 +1,12 @@
+package com.creospan.rpi.setexample;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimSetTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
