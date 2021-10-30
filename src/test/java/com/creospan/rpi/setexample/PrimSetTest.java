513d5a2a44e092c29057d342ea6ec13276a91591
Terry Trippany <terry.trippany@gmail.com>
2021-10-30 16:12:26 -0500

    Initial Test Class Organized with Empty Test Methods

--- src/test/java/com/creospan/rpi/setexample/PrimSetTest.java
+++ src/test/java/com/creospan/rpi/setexample/PrimSetTest.java
@@ -1,12 +1,45 @@
 package com.creospan.rpi.setexample;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PrimSetTest {
-
     @BeforeEach
     void setUp() {
+
+    }
+
+    @Test
+    void setCanHoldAnObject() {
+    }
+
+    @Test
+    void setCannotContainNulls() {
+    }
+
+    @Test
+    void setCannotHoldDuplicates() {
+    }
+
+    @Test
+    void setCanHoldManyObjects() {
+    }
+
+    @Test
+    void setIsEmpty() {
+    }
+
+    @Test
+    void setCanBeCleared() {
+    }
+
+    @Test
+    void objectsCanBeRemovedFromSet() {
     }
 }
