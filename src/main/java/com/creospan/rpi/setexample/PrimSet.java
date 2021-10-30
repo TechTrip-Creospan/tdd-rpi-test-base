6f5d91792aa18705e1e9ccbc4efefef0bcdf69c1
Terry Trippany <terry.trippany@gmail.com>
2021-10-30 16:16:00 -0500

    setCanHoldAnObject() - TDD Red

--- src/main/java/com/creospan/rpi/setexample/PrimSet.java
+++ src/main/java/com/creospan/rpi/setexample/PrimSet.java
@@ -2,4 +2,10 @@ package com.creospan.rpi.setexample;
 
 public class PrimSet {
 
+    public void add(String someString) {
+    }
+
+    public boolean contains(String someString) {
+        return false;
+    }
 }
